6a6f30894285cfaf5f034e2c2b51570ee25333d5
Imantha Thuduwage <imantha.official@gmail.com>
2023-07-10 14:47:27 +0530

    users section fine tuned

--- users/editUser.php
+++ users/editUser.php
@@ -4,16 +4,20 @@
 
 <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
     <div class="d-flex p-2 justify-content-between flex-wrap flex-md-nowrap align-items-center" id="form-header">
-        <h4>Add New User Account</h4>
+        <h4>Update New Details</h4>
         <div>
-            <button type="button" class="btn btn-sm px-5-bottom border-end border-2" onclick="document.location='<?= SYSTEM_PATH; ?>users/user.php'">
+            <button type="button" class="btn btn-sm px-4 border-bottom border-end border-2" onclick="document.location='<?= SYSTEM_PATH; ?>users/user.php'">
+                <img src="<?= SYSTEM_PATH; ?>assets/icons/eye.png" class="me-2">
                 View Users
-            </button>
-            <button type="button" class="btn btn-sm px-5 border-bottom border-end border-2">
-                Filters
             </button>
         </div>
     </div>
+
+    <style>
+        #form-header>h4 {
+            padding-right: 700px !important;
+        }
+    </style>
 
     <?php
     if ($_SERVER['REQUEST_METHOD'] == 'GET') {
@@ -42,7 +46,7 @@
                             <h6 class="pt-3 pb-2 mb-0">User Account Details</h6>
                         </div>
                     </div>
-                    <input type="hidden"  name="userId" value="<?php echo $userId; ?>">
+                    <input type="hidden" name="userId" value="<?php echo $userId; ?>">
                     <div class="row justify-content-start gx-5">
                         <div class="col-sm">
                             <div class="row row-cols-2 row-cols-lg-1">

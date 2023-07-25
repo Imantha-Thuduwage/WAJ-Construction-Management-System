bc89520491205d2aa2fe9b7fcf97df1929f0acc5
Imantha Thuduwage <imantha.official@gmail.com>
2023-07-25 16:10:33 +0530

    admin dashboard completed

--- users/dashboard/admin.php
+++ users/dashboard/admin.php
@@ -2,113 +2,58 @@
 
 <?php
 
-// Get Today Date to Filter Overdue Tasks
+// Get Today Date
 $currentDate = date("Y-m-d");
 
-// Get Today Date to Filter Overdue Tasks
+// Get Today Month
 $thismonth = date("m");
 
-// Get Today Year to Filter Overdue Tasks
+// Get Today Year
 $currentYear = date("Y");
 
-// SQL Query for Get Count of All Projects 
-$sql = "SELECT COUNT(`project_id`) AS project_count FROM tbl_project";
+// SQL Query for Get Count of All User Accounts
+$sql = "SELECT COUNT(`user_id`) AS user_count FROM tbl_user";
 $db = dbConn();
 $result = $db->query($sql);
 
 if ($result) {
   $row = $result->fetch_assoc();
-  $projectCount = $row['project_count'];
-} else {
-  echo "Error executing the query: " . $db->error;
+  $userCount = $row['user_count'];
 }
 
-// SQL Query for Get Count of All Completed Projects
-$sql = "SELECT COUNT(DISTINCT project_id) AS completed_projects_count
-FROM tbl_schedule_task WHERE current_status <> 1 AND current_status <> 2 AND current_status <> 3 AND current_status <> 5";
+// SQL Query for Get Count of All Deactive Users
+$sql = "SELECT COUNT(user_id) AS active_user_accounts
+FROM tbl_user WHERE `status` = 1 ";
 $db = dbConn();
 $result = $db->query($sql);
 
 if ($result) {
   $row = $result->fetch_assoc();
-  $completedCount = $row['completed_projects_count'];
-} else {
-  echo "Error executing the query: " . $db->error;
+  $activedAcc = $row['active_user_accounts'];
 }
 
-// SQL Query for Get Count of All Completed Projects
-$sql = "SELECT COUNT(task_id) AS overdue_count
-FROM tbl_schedule_task WHERE ending_date < '$currentDate'";
+// SQL Query for Get Count of All Deactive Users
+$sql = "SELECT COUNT(user_id) AS deactivated_accounts
+FROM tbl_user WHERE `status` = 0 ";
 $db = dbConn();
 $result = $db->query($sql);
 
 if ($result) {
   $row = $result->fetch_assoc();
-  $overdueCount = $row['overdue_count'];
-} else {
-  echo "Error executing the query: " . $db->error;
+  $deactivatedAcc = $row['deactivated_accounts'];
 }
 
-// SQL Query for Get Count of All Completed Projects
-$sql = "SELECT COUNT(task_id) as overdue_count
-FROM tbl_schedule_task WHERE ending_date = '$thismonth'";
+// Query to retrieve monthly headcount growth
+$sql = "SELECT MONTH(add_date) AS month, COUNT(*) AS total_users
+        FROM tbl_user
+        GROUP BY MONTH(add_date)
+        ORDER BY MONTH(add_date)";
 $db = dbConn();
 $result = $db->query($sql);
-
-if ($result) {
-  $row = $result->fetch_assoc();
-  $overdueCount = $row['overdue_count'];
-} else {
-  echo "Error executing the query: " . $db->error;
-}
-
-// SQL Query for get data to showing completed project using Chart
-// SQL Query for Get Count of All Completed Projects
-$sql = "SELECT COUNT(DISTINCT project_id) AS completed_projects_count
-FROM tbl_schedule_task WHERE current_status <> 1 AND current_status <> 2 AND current_status <> 3 AND current_status <> 5";
-$db = dbConn();
-$result = $db->query($sql);
-
-if ($result) {
-  $row = $result->fetch_assoc();
-  $completedCount = $row['completed_projects_count'];
-}
-
-$sql = "SELECT COUNT(*) as all_projects FROM tbl_project";
-$db = dbConn();
-$result = $db->query($sql);
-
-if ($result) {
-  $row = $result->fetch_assoc();
-  $allProjects = $row['all_projects'];
-}
-
-// Calculate the progress percentage
-$completed = ($allProjects > 0) ? (($completedCount / $allProjects) * 100) : 0;
-
-// Pass the progress value to JavaScript
-echo '<script>const projects = ' . $completed . ';</script>';
-
-// Query for get Task Count for Each Project
-$sql = "SELECT project_id, COUNT(*) AS task_count
-        FROM tbl_schedule_task
-        WHERE YEAR(add_date) >= '$currentYear' AND MONTH(add_date) >= '$thismonth'
-        GROUP BY project_id
-        ORDER BY project_id DESC
-        LIMIT 10";
-$db = dbConn();
-
-$result = $db->query($sql);
-
-// Step 3: Fetch the results and store them in variables
-$projectIDs = [];
-$taskCounts = [];
-
-if ($result) {
-  while ($row = $result->fetch_assoc()) {
-    $projectIDs[] = $row['project_id'];
-    $taskCounts[] = $row['task_count'];
-  }
+// Fetch the data into an array
+$data = array();
+while ($row = $result->fetch_assoc()) {
+  $data[$row['month']] = $row['total_users'];
 }
 
 ?>
@@ -118,266 +63,110 @@ if ($result) {
   <div class="container field p-3">
     <div class="row justify-content-start gx-5">
       <div class="col-sm">
-        <div class="row row-cols-2 row-cols-lg-1">
+        <div class="row row-cols-2 row-cols-lg-1 justify-content-between">
           <div class="col-3 card shadow m-3 custom-shadow" onclick="document.location='<?= SYSTEM_PATH; ?>projects/project.php'">
             <div class="card-body">
-              <h5 for="project_name" class="mb-2">Total Projects</h5>
-              <h4><?= $projectCount ?></h4>
+              <h5 for="project_name" class="mb-2" style="color:#2784ff">Total Users</h5>
+              <h4><?= $userCount ?></h4>
             </div>
           </div>
-          <div class="col-3 card shadow m-3 custom-shadow" onclick="document.location='<?= SYSTEM_PATH; ?>projects/project.php'">
+          <div class="col-4 card shadow m-3 custom-shadow" onclick="document.location='<?= SYSTEM_PATH; ?>projects/project.php'">
             <div class="card-body">
-              <h6 for="project_name" class="mb-2">Total Projects Completed</h6>
-              <h4><?= $completedCount ?></h4>
+              <h5 for="project_name" class="mb-2" style="color:green">Total Acitive User Accounts</h5>
+              <h4><?= $activedAcc ?></h4>
             </div>
           </div>
           <div class="col-4 card shadow m-3 custom-shadow" onclick="document.location='<?= SYSTEM_PATH; ?>schedules/schedule.php'">
-            <div class="card-body">
-              <h6 for="project_name" class="mb-2">Amount of Overdue Tasks to Today</h6>
-              <h4><?= $completedCount ?></h4>
+            <div class="card-body border-danger">
+              <h5 for="project_name" class="mb-2" style="color:red">Total Deactivated User Accounts</h5>
+              <h4><?= $deactivatedAcc ?></h4>
             </div>
           </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="row justify-content-start gx-5">
-      <div class="col-sm-5">
-        <div class="border bg-light">
-          <div class="card id-section text-center" onclick="document.location='<?= SYSTEM_PATH; ?>projects/project.php'">
-            <div class="card-body" style=" display: flex; justify-content: center; align-items: center;">
-              <div class="p-1 border border-0">
-                <canvas id="project-chart"></canvas>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col-7">
-        <div class="row row-cols-2 row-cols-lg-1 g-2 g-lg-3">
-          <div class="col-sm border" style="height: 345px; overflow: auto;" onclick="document.location='<?= SYSTEM_PATH; ?>schedules/schedule.php'">
-            <h4 for="project_name" class="my-4">Task List, Deadline Coming This Month</h4>
-            <div class="table-responsive">
-              <?php
-              // Get Today Date to Filter Overdue Tasks
-              $thismonth = date("m");
-
-              $sql = "SELECT `task_id`, `project_id`, `task_name`, `ending_date` 
-                FROM tbl_schedule_task WHERE YEAR(`ending_date`) = '$currentYear' AND MONTH(ending_date) = $thismonth";
-
-              // Calling to the Connection
-              $db = dbConn();
-
-              // Get Result
-              $result = $db->query($sql);
-              ?>
-              <table class="table table-sm custom-shadow-red">
-                <thead class="shadow-lg">
-                  <tr>
-                    <th scope="col">Task ID</th>
-                    <th scope="col">Project ID</th>
-                    <th scope="col">Task Name</th>
-                    <th scope="col">Ending Date</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <?php
-                  if ($result->num_rows > 0) {
-                    while ($row = $result->fetch_assoc()) {
-
-                  ?>
-                      <tr class="shadow-lg">
-                        <td class="align-middle"><?= $row['task_id']; ?></td>
-                        <td class="align-middle"><?= $row['project_id']; ?></td>
-                        <td class="align-middle"><?= $row['task_name']; ?></td>
-                        <td class="align-middle"><?= $row['ending_date']; ?></td>
-                      </tr>
-                  <?php
-                    }
-                  }
-                  ?>
-                </tbody>
-              </table>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="row justify-content-start gx-5">
-      <div class="col-sm my-4">
-        <div class="border bg-light">
-          <div class="card id-section text-center" onclick="document.location='<?= SYSTEM_PATH; ?>schedules/schedule.php'">
-            <div class="card-body" style=" display: flex; justify-content: center; align-items: center;">
-              <div class="p-1 border border-0">
-                <canvas id="task-count"></canvas>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="row justify-content-start gx-5">
-      <div class="col-6" onclick="document.location='<?= SYSTEM_PATH; ?>schedules/schedule.php'">
-        <h4 for="project_name" class="my-4">Overdue Task List to Today</h4>
-        <div class="table-responsive custom-shadow-red">
-          <?php
-          // Get Today Date to Filter Overdue Tasks
-          $todayDate = date("Y-m-d");
-
-          $sql = "SELECT `task_id`, `project_id`, `task_name`, `ending_date` 
-              FROM tbl_schedule_task WHERE ending_date < '$todayDate'";
-
-          // Calling to the Connection
-          $db = dbConn();
-
-          // Get Result
-          $result = $db->query($sql);
-          ?>
-          <table class="table table-sm custom-shadow-red">
-            <thead class="shadow-lg">
-              <tr>
-                <th scope="col">Task ID</th>
-                <th scope="col">Project ID</th>
-                <th scope="col">Task Name</th>
-                <th scope="col">Ending Date</th>
-              </tr>
-            </thead>
-            <tbody>
-              <?php
-              if ($result->num_rows > 0) {
-                while ($row = $result->fetch_assoc()) {
-
-              ?>
-                  <tr class="shadow-lg">
-                    <td class="align-middle"><?= $row['task_id']; ?></td>
-                    <td class="align-middle"><?= $row['project_id']; ?></td>
-                    <td class="align-middle"><?= $row['task_name']; ?></td>
-                    <td class="align-middle"><?= $row['ending_date']; ?></td>
-                  </tr>
-              <?php
-                }
-              }
-              ?>
-            </tbody>
-          </table>
-        </div>
-      </div>
-      <div class="col-6" onclick="document.location='<?= SYSTEM_PATH; ?>schedules/schedule.php'">
-        <h4 for="project_name" class="my-4">Project List of Estimated to End This Month</h4>
-        <div class="table-responsive">
-          <?php
-
-          $sql = "SELECT `project_id`, `project_name`, `end_date` 
-              FROM tbl_project WHERE YEAR(`end_date`) = '$currentYear' AND MONTH(`end_date`) = '$thismonth'";
-
-          // Calling to the Connection
-          $db = dbConn();
-
-          // Get Result
-          $result = $db->query($sql);
-          ?>
-          <table class="table table-sm custom-shadow-red">
-            <thead class="shadow-lg">
-              <tr>
-                <th scope="col">Project ID</th>
-                <th scope="col">Project Name</th>
-                <th scope="col">Ending Date</th>
-              </tr>
-            </thead>
-            <tbody>
-              <?php
-              if ($result->num_rows > 0) {
-                while ($row = $result->fetch_assoc()) {
-
-              ?>
-                  <tr class="shadow-lg">
-                    <td class="align-middle"><?= $row['project_id']; ?></td>
-                    <td class="align-middle"><?= $row['project_name']; ?></td>
-                    <td class="align-middle"><?= $row['end_date']; ?></td>
-                  </tr>
-              <?php
-                }
-              }
-              ?>
-            </tbody>
-          </table>
         </div>
       </div>
     </div>
   </div>
 
+  <div class="row justify-content-start gx-5">
+    <div class="col-9 mx-auto">
+      <div class="card id-section text-center mb-3" onclick="document.location='<?= SYSTEM_PATH; ?>schedules/schedule.php'">
+        <div class="card-body" style=" display: flex; justify-content: center; align-items: center;">
+          <div class="p-1 border border-0" style="width: 100%;">
+            <canvas id="lineChart"></canvas>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </main>
 
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
 <script>
-  // Access the projectProgress variable from PHP
-  const progress = projects;
+  // Get current year and months
+  var currentDate = new Date();
+  var currentYear = currentDate.getFullYear();
+  var months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ];
 
-  // Create the chart
-  const ctx = document.getElementById('project-chart').getContext('2d');
+  // Get data from PHP
+  var data = <?php echo json_encode(array_values($data)); ?>;
 
-  const data = {
-    labels: ['Progress', 'Remaining'],
-    datasets: [{
-      data: [progress, 100 - progress],
-      backgroundColor: ['#002158', '#f0e51a'],
-      borderWidth: 0
-    }]
-  };
-
-  const config = {
-    type: 'doughnut',
-    data: data,
-    options: {
-      responsive: true,
-      plugins: {
-        title: {
-          display: true,
-          text: 'Progress of Project'
-        }
-      }
-    }
-  };
-
-  new Chart(ctx, config);
-</script>
-
-<script>
-  // Retrieve data from the database (replace with your query and actual data)
-  // Use the retrieved data to create a bar chart using Chart.js
-  var projectIDs = <?php echo json_encode($projectIDs); ?>;
-  var taskCounts = <?php echo json_encode($taskCounts); ?>;
-
-  // Step 2: Create a bar chart using Chart.js
-  var id = document.getElementById('task-count').getContext('2d');
-  var chart = new Chart(id, {
-    type: 'bar',
-    data: {
-      labels: projectIDs,
-      datasets: [{
-        label: 'Task Count',
-        data: taskCounts,
-        backgroundColor: '#e014cf', // Change the color as desired
-        borderWidth: 1,
-      }]
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true,
-          ticks: {
-            stepSize: 1, // Set the step size to 1 to display whole numbers
-            precision: 0 // Set the precision to 0 to remove decimal places
+  // Create the line chart
+  document.addEventListener("DOMContentLoaded", function() {
+    var ctx = document.getElementById("lineChart").getContext("2d");
+    var lineChart = new Chart(ctx, {
+      type: "line",
+      data: {
+        labels: months,
+        datasets: [{
+          label: "Total Head Count",
+          data: data,
+          borderColor: "rgb(75, 192, 192)",
+          fill: false
+        }]
+      },
+      options: {
+        responsive: true,
+        plugins: {
+          legend: {
+            display: true,
+          }
+        },
+        scales: {
+          x: {
+            grid: {
+              display: false // Remove vertical grid lines
+            }
+          },
+          y: {
+            grid: {
+              display: false // Remove horizontal grid lines
+            },
+            beginAtZero: true,
+            ticks: {
+              stepSize: 1,
+              callback: function(value) {
+                if (Number.isInteger(value)) {
+                  return value;
+                }
+              }
+            }
           }
         }
       }
-    },
-    dataset: {
-      barPercentage: 0.5, // Adjust the bar width to be half of the default width
-      categoryPercentage: 1.0 // Keep the category width the same as the default width
-    }
+    });
   });
 </script>

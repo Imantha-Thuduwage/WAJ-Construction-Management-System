05d0418cf1fb009adcbf1a4fd56efd6a7fcc9421
Imantha Thuduwage <imantha.official@gmail.com>
2023-08-03 19:19:31 +0530

    Maintenance filters

--- maintenance/tools/getFilteredRecord.php
+++ maintenance/tools/getFilteredRecord.php
@@ -11,8 +11,8 @@ $where = "";
 if (!empty($maintenanceId)) {
     $where .= "maintenance_id = '$maintenanceId' AND ";
 }
-if (!empty($machineId)) {
-    $where .= "machine_id = '$machineId' AND ";
+if (!empty($toolId)) {
+    $where .= "tool_id = '$toolId' AND ";
 }
 if (!empty($startDate) && !empty($endDate)) {
     $where .= "maintenance_date BETWEEN '$startDate' AND '$endDate' AND ";
@@ -22,7 +22,7 @@ if (!empty($where)) {
     $where = "WHERE $where";
 }
 
-$sql = "SELECT * FROM tbl_machine_maintenance $where";
+$sql = "SELECT * FROM tbl_tool_maintenance $where";
 $db = dbConn();
 $result = $db->query($sql);
 
@@ -35,7 +35,7 @@ if ($result->num_rows > 0) {
 ?>
         <tr class="shadow-sm">
             <td class="align-middle"><?= $row['maintenance_id']; ?></td>
-            <td class="align-middle"><?= $row['machine_id']; ?></td>
+            <td class="align-middle"><?= $row['tool_id']; ?></td>
             <td class="align-middle"><?= $row['maintenance_date']; ?></td>
             <td>
                 <button type="button" class="btn btn-outline-info btn-sm" onclick="document.location='editMaintenance.php?maintenance_id=<?= $row['maintenance_id']; ?>'">
